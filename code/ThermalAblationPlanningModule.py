65c44b68d65b6d50b94e0e5671787df1374e3bf9
haichao <hmiao87@gmail.com>
2011-11-18 18:21:13 +0100

    gui started

--- code/ThermalAblationPlanningModule.py
+++ code/ThermalAblationPlanningModule.py
@@ -5,6 +5,7 @@ from __main__ import vtk, qt, ctk, slicer
 #
 
 class ThermalAblationPlanningModule:
+
   def __init__(self, parent):
     parent.title = "Thermal Ablation Planning"
     parent.category = "Thermal Ablation"
@@ -14,7 +15,7 @@ class ThermalAblationPlanningModule:
     For more information, read the README file.
     """
     parent.acknowledgementText = """
-    This module was created by Haichao Miao as a part of his bachelor thesis under the guidance of Dr. Wolfgang Schramm
+    This module was created by Haichao Miao as a part of his bachelor thesis under the guidance of Wolfgang Schramm, PhD 
     at the Vienna University of Technology.
     """
     self.parent = parent
@@ -24,6 +25,8 @@ class ThermalAblationPlanningModule:
 #
 
 class ThermalAblationPlanningModuleWidget:
+    
+    
   def __init__(self, parent = None):
     if not parent:
       self.parent = slicer.qMRMLWidget()
@@ -37,40 +40,125 @@ class ThermalAblationPlanningModuleWidget:
       self.parent.show()
     
   def setup(self):
-    # Instantiate and connect widgets ...
-    
+
+    #
     # Probe Placement Planning Collapsible button
+    #
+    
     probePlacementPlanningCollapsibleButton = ctk.ctkCollapsibleButton()
     probePlacementPlanningCollapsibleButton.text = "Probe Placement Planning"
     self.layout.addWidget(probePlacementPlanningCollapsibleButton)
     
-
     formLayout = qt.QFormLayout(probePlacementPlanningCollapsibleButton)
-    
 
-    # Apply button
-    applyButton = qt.QPushButton("Apply")
-    applyButton.toolTip = "Execute the placement of the probe"
-    formLayout.addWidget(applyButton)
-    applyButton.connect('clicked(bool)', self.onApplyButtonClicked)
+    # ROI Placement
+    
+    roiPlacementGroupBox = qt.QGroupBox()
+    roiPlacementGroupBox.setTitle("1: ROI Placement")
+    self.roiPlacementGroupBox = roiPlacementGroupBox
+    
+    formLayout.addWidget(roiPlacementGroupBox)
+    
+    roiPlacementLayout = qt.QFormLayout(roiPlacementGroupBox)
+    
+    # todo: value -> instance variable
+    roiPlacementLineEdit = qt.QLineEdit()
+    roiPlacementLineEdit.text = "100"
+    self.roiPlacementLineEdit = roiPlacementLineEdit
+    
+    addInsertionRadiusButton = qt.QPushButton("Add Insertion Radius")
+    addInsertionRadiusButton.toolTip = "Add Insertion Radius"
+    
+    addInsertionRadiusButton.connect('clicked(bool)', self.onAddInsertionRadiusButtonClicked)
+    
+    roiPlacementLayout.addRow(roiPlacementLineEdit, addInsertionRadiusButton)
+    
+    self.addInsertionRadiusButton = addInsertionRadiusButton
+    
+    # 2a: Point Selection
+
+    pointSelectionGroupBox = qt.QGroupBox()
+    pointSelectionGroupBox.setTitle("2a: Point Selection")
+    self.pointSelectionGroupBox = pointSelectionGroupBox
+    
+    formLayout.addWidget(pointSelectionGroupBox)
+    
+    pointSelectionLayout = qt.QFormLayout(pointSelectionGroupBox)
+    
+    pointOneComboBox = slicer.qMRMLNodeComboBox()
     
     
     
-    # Set local var as instance attribute
-    self.applyButton = applyButton
+    pointTwoComboBox = qt.QComboBox()
     
-    # Default Button
-    defaultButton = qt.QPushButton("Default")
-    defaultButton.toolTip = "Reset the parameters to default"
-    formLayout.addWidget(defaultButton)
-    defaultButton.connect('clicked(bool)', self.onDefaultButtonClicked)
+    # todo: fill with point selection
+    self.pointOneComboBox = pointOneComboBox
+    self.pointTwoComboBox = pointTwoComboBox
     
-    self.defaultButton = defaultButton
+    pointSelectionLayout.addRow(pointOneComboBox, pointTwoComboBox)
+    
+    # 2b: Probe Placement
+    
+    probePlacementGroupBox = qt.QGroupBox()
+    probePlacementGroupBox.setTitle("2b: Probe Placement")
+    self.probePlacementGroupBox = probePlacementGroupBox
+    
+    formLayout.addWidget(probePlacementGroupBox)
+    
+    probePlacementLayout = qt.QFormLayout(probePlacementGroupBox)
+    
+    addProbeButton = qt.QPushButton("Add Probe")
+    addProbeButton.toolTip = "Add Probe"
+    self.addProbeButton = addProbeButton
+    
+    addProbeButton.connect('clicked(bool)', self.onAddProbeButtonClicked)
+   
+    probeLineEdit = qt.QLineEdit()
+    # todo: value -> instance variable
+    probeLineEdit.text = "Probe 1"
+    
+    self.probeLineEdit = probeLineEdit
+    
+    probePlacementLayout.addRow(addProbeButton, probeLineEdit)
+    
+    # 3: Ablation Zone Estimation
+    
+    ablationZoneEstimationGroupBox = qt.QGroupBox()
+    ablationZoneEstimationGroupBox.setTitle("3: Ablation Zone Estimation")
+    self.ablationZoneEstimationGroupBox = ablationZoneEstimationGroupBox
+    
+    formLayout.addWidget(ablationZoneEstimationGroupBox)
+    
+    ablationZoneEstimationLayout = qt.QFormLayout(ablationZoneEstimationGroupBox)
     
     
-  def onApplyButtonClicked(self):
-    print "Apply Button clicked!"
     
-  def onDefaultButtonClicked(self):
-    print "Default Button clicked!"
+    devicesComboBox = qt.QComboBox()
+
+    self.devices = ["Galil ICE Sphere", "Galil ICE Rod", "Galil ICE Seed", "Rita Stardust XL", "Radionics CoolTip", "Radionics Cluster Probe"]
+    
+    for device in self.devices:
+      devicesComboBox.addItem(device)
+    
+    self.devicesComboBox = devicesComboBox
+    
+    drawAblationZoneButton = qt.QPushButton("Draw Ablation Zone")
+    drawAblationZoneButton.toolTip = "Draw Ablation Zone"
+    self.drawAblationZoneButton = drawAblationZoneButton
+    
+    drawAblationZoneButton.connect('clicked(bool)', self.onDrawAblationZoneButtonClicked)
+    
+    ablationZoneEstimationLayout.addRow("Select Device: ", devicesComboBox)
+    ablationZoneEstimationLayout.addRow(drawAblationZoneButton)
+    
+  def onAddInsertionRadiusButtonClicked(self):
+    print "Add Insertion Radius "
+
+  def onAddProbeButtonClicked(self):
+    print "Add Probe"
+
+  def onDrawAblationZoneButtonClicked(self):
+    print "Draw Ablation Zone"
+
+  
 
