e0bb2835e4cf89a749e790c5a69d4180dc9befa9
haichao <hmiao87@gmail.com>
2012-03-26 15:49:24 +0200

    - xml schema prototype for devices defined - xml datastructure for devices - xml parsing - creating a 3D sphere is now possible

--- code/ThermalAblationPlanningModule.py
+++ code/ThermalAblationPlanningModule.py
@@ -1,6 +1,7 @@
 from __main__ import vtk, qt, ctk, slicer
+from xml.dom.minidom import parseString
 
-#
+
 # ThermalAblationPlanningModule
 #
 
@@ -11,11 +12,11 @@ class ThermalAblationPlanningModule:
     parent.category = "Ablation"
     parent.contributor = "Haichao Miao <hmiao87@gmail.com>"
     parent.helpText = """
-    Slicer 4 module for planning a ablation procedure.
+    Slicer 4 module for planning a thermal ablation procedure.
     For more information, read the README file.
     """
     parent.acknowledgementText = """
-    This module was created by Haichao Miao as a part of his bachelor thesis under the guidance of Wolfgang Schramm, PhD 
+    This module was created by Haichao Miao as a part of his bachelor thesis under the guidance of Dr. Wolfgang Schramm 
     at the Vienna University of Technology.
     """
     self.parent = parent
@@ -28,13 +29,16 @@ class ThermalAblationPlanningModuleWidget:
     
     
   def __init__(self, parent = None):
+    
     if not parent:
       self.parent = slicer.qMRMLWidget()
       self.parent.setLayout(qt.QVBoxLayout())
       self.parent.setMRMLScene(slicer.mrmlScene)
     else:
       self.parent = parent
+      
     self.layout = self.parent.layout()
+    
     if not parent:
       self.setup()
       self.fiducialsNodeSelector.setMRMLScene(slicer.mrmlScene)
@@ -43,6 +47,7 @@ class ThermalAblationPlanningModuleWidget:
       
     
   def setup(self):
+    
 
     #
     # Probe Placement Planning Collapsible button
@@ -64,20 +69,6 @@ class ThermalAblationPlanningModuleWidget:
     
     roiPlacementLayout = qt.QFormLayout(roiPlacementGroupBox)
 
-    self.inputVolumeNodeSelector = slicer.qMRMLNodeComboBox()
-    self.inputVolumeNodeSelector.objectName = 'inputVolumeNodeSelector'
-    self.inputVolumeNodeSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
-    self.inputVolumeNodeSelector.noneEnabled = False
-    self.inputVolumeNodeSelector.addEnabled = False
-    self.inputVolumeNodeSelector.removeEnabled = False
-    self.inputVolumeNodeSelector.addAttribute("vtkMRMLScalarVolumeNode", "LabelMap", "0")    
-    roiPlacementLayout.addRow("Input Volume:", self.inputVolumeNodeSelector)
-    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)',
-                        self.inputVolumeNodeSelector, 'setMRMLScene(vtkMRMLScene*)')
-    #self.inputVolumeNodeSelector.connect('currentNodeChanged(vtkMRMLNode*)', self.onInputVolumeChanged)
-    #self.inputVolumeNodeSelector.connect('nodeActivated(vtkMRMLNode*)', self.onInputVolumeChanged)
-    
-
     self.fiducialsNodeSelector = slicer.qMRMLNodeComboBox()
     self.fiducialsNodeSelector.objectName = "fiducialsNodeSelector"
     self.nodeTypes = ['vtkMRMLAnnotationHierarchyNode']
@@ -89,7 +80,7 @@ class ThermalAblationPlanningModuleWidget:
     roiPlacementLayout.addRow("Target Point:", self.fiducialsNodeSelector)
     self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)',
                         self.fiducialsNodeSelector, 'setMRMLScene(vtkMRMLScene*)')     
-    
+     
     
     
     # todo: value -> instance variable
@@ -186,9 +177,96 @@ class ThermalAblationPlanningModuleWidget:
     currentValumeNode = self.inputVolumeNodeSelector.currentNode()
     currentTargetNode = self.fiducialsNodeSelector.currentNode()
     
+    newModelNode = slicer.mrmlScene.CreateNodeByClass("vtkMRMLModelNode")
+    newModelNode.SetScene(slicer.mrmlScene)
+    newModelNode.SetName(slicer.mrmlScene.GetUniqueNameByString(self.outputModelNodeSelector.baseName))        
+    slicer.mrmlScene.AddNode(newModelNode)
+    currentModelNode = newModelNode
+    self.outputModelNodeSelector.setCurrentNode(currentModelNode)        
+    
+  
+  def onAddInsertionRadiusButtonClicked(self):
+    print "Add Insertion Radius"
+    
+    file = open('/Users/haichao/git/Slicer4-Module/code/devices.xml', 'r')
+    
+    data = file.read()
+    
+    dom = parseString(data)
+    
+    xmlTag = dom.getElementsByTagName('devices')[0].toxml()
+    
+    xmlData = xmlTag.replace('<devices>','').replace('</devices>','')
+    
+    print xmlData
+    
+    model = TestSphere(self.fiducialsNodeSelector.currentNode())
     
   def onAddProbeButtonClicked(self):
     print "Add Probe"
 
   def onDrawAblationZoneButtonClicked(self):
     print "Draw Ablation Zone"
+
+class TestSphere:
+  
+  def __init__(self, fiducialListNode):
+    fids = fiducialListNode
+    scene = slicer.mrmlScene
+
+    # Create model node
+    model = slicer.vtkMRMLModelNode()
+    model.SetScene(scene)
+    model.SetName("Path-%s" % fids.GetName())
+    
+
+    # Create display node
+    modelDisplay = slicer.vtkMRMLModelDisplayNode()
+    #modelDisplay.SetColor(1,1,0) # yellow
+    modelDisplay.SetScene(scene)
+    scene.AddNodeNoNotify(modelDisplay)
+    model.SetAndObserveDisplayNodeID(modelDisplay.GetID())
+
+    # Add to scene
+    modelDisplay.SetPolyData(model.GetPolyData())
+    scene.AddNode(model)
+
+    # Camera cursor
+    sphere = vtk.vtkSphereSource()
+    sphere.SetRadius(100)
+    sphere.Update()
+
+
+    # Create model node
+    cursor = slicer.vtkMRMLModelNode()
+    cursor.SetScene(scene)
+    cursor.SetName("Cursor-%s" % fids.GetName())
+    cursor.SetAndObservePolyData(sphere.GetOutput())
+
+    # Create display node
+    cursorModelDisplay = slicer.vtkMRMLModelDisplayNode()
+    cursorModelDisplay.SetColor(1,1,0) # red
+    cursorModelDisplay.SetScene(scene)
+    scene.AddNodeNoNotify(cursorModelDisplay)
+    cursor.SetAndObserveDisplayNodeID(cursorModelDisplay.GetID())
+
+    # Add to scene
+    cursorModelDisplay.SetPolyData(sphere.GetOutput())
+    scene.AddNode(cursor)
+
+    # Create transform node
+    transform = slicer.vtkMRMLLinearTransformNode()
+    transform.SetName('Transform-%s' % fids.GetName())
+    
+    scene.AddNode(transform)
+    cursor.SetAndObserveTransformNodeID(transform.GetID())
+    
+    # Translation
+    transformMatrix = vtk.vtkMatrix4x4()
+    transformMatrix.SetElement(0,3, 100)
+    transformMatrix.SetElement(1,3, 100)
+    
+    transform.ApplyTransformMatrix(transformMatrix)
+    
+    self.transform = transform
+    
